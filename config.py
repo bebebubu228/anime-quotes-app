acd33b5a4c2d1e17eaa9c939de3d718efe40804e
bebebubu228 <tea80083@gmail.com>
2025-11-03 19:35:15 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 #настройка бд по тз
-SERVER = 'mysql4.joinserver.xyz'
-DATABASE = 's410037_NKEiVT1'
-USERNAME = 'u410037_ihEsziK5yx'
-PASSWORD = 'hnOw+LKzGcHrMtLt!QU5=A=w'
+SERVER = 
+DATABASE = 
+USERNAME = 
+PASSWORD = 
 
 #настройка апи
 ANIMECHAN_API = 'https://api.animechan.io/v1'
