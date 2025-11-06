7e2077e9c456d91a2bf036d9b1a246487108816c
bebebubu228 <tea80083@gmail.com>
2025-11-07 02:22:23 +0700

    Add a registration function

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_bcrypt import Bcrypt
 import requests
 import mysql.connector
 import os
@@ -15,8 +16,8 @@ DB_PORT = os.getenv('DB_PORT')
 
 URL = 'https://api.animechan.io/v1'
 
-def load_quotes():
 
+def load_quotes():
     params = {
         "anime" : "Cowboy Bebop",
         "page" : 1
@@ -36,7 +37,6 @@ def load_quotes():
         print(f"полученный ответ: {json_in}")
         return
 
-
     data_to_insert =[]
     json_quotes = json_in['data']
 
@@ -44,8 +44,8 @@ def load_quotes():
         row_data = (
             qoute_data.get('content'),
             qoute_data.get('anime',{}).get('name'),
-            qoute_data.get('character', {}).get('name')  
-    )
+            qoute_data.get('character', {}).get('name') 
+        )
         data_to_insert.append(row_data)
 
     
@@ -63,7 +63,6 @@ def load_quotes():
         return
     except mysql.connector.Error as e:
         print(f"ошибка:{e}")
-
     finally:
         if cursor:
             cursor.close()
@@ -109,39 +108,68 @@ def get_quotes_of_day():
         return daily_cash
     else:
         return[]
-
-
-# def form_auth():
-#     # data = []
-#     cnx = None
-#     cursor = None
-#     if request.method == 'POST':
-#         user_name = request.form.get('user_name')
-#         email = request.form.get('email')
-#         password = request.form.get('password')
-#     try:
-#         cnx = mysql.connector.connect(
-#             host = DB_HOST, user = DB_USER, password = DB_PASSWORD, database = DATABASE, port = DB_PORT
-#     )
-#         cursor = cnx.cursor(dictionary=True)
-#         rqst= "SELECT username, email, password_hash FROM users"
-#         cursor.execute(rqst)
-#         data = cursor.fetchall()
-#     except mysql.connector.Error as e:
-#         print(f"ошибка:{e}")
-#     finally:
-#         if cursor:
-#             cursor.close()
-#         if cnx and cnx.is_connected():
-#             cnx.close()
-#     try:
-#         if data[0][0] != user_name and email and password:
-#             print("неверный логин или пароль")
-#             return 
-#     except:
+ 
+def reg_form(username, email, password):
+    cnx = None
+    cursor = None
+    password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+    try:
+        cnx = mysql.connector.connect(
+            host = DB_HOST, user = DB_USER, password = DB_PASSWORD, database = DATABASE, port = DB_PORT
+        )
+        cursor = cnx.cursor()
+        rqst_select = "SELECT id FROM users WHERE username = %s OR email = %s"
+        cursor.execute(rqst_select, (username, email)) 
+        
+        if cursor.fetchone():
+            return "Пользователь с таким именем или email уже существует", False 
+        rqst_insert = "INSERT INTO users(username, email, password_hash) VALUES(%s, %s, %s)"
+        cursor.execute(rqst_insert, (username, email, password_hash))
+        cnx.commit() 
+        return "Регистрация успешна! Теперь вы можете войти.", True 
     
-    # return data
-    
+    except mysql.connector.Error as e:
+        print(f"Ошибка базы данных при регистрации: {e}")
+        return f"Произошла ошибка базы данных: {e}", False 
+    except Exception as e:
+        print(f"Непредвиденная ошибка при регистрации: {e}")
+        return f"Произошла непредвиденная ошибка: {e}", False
+    finally:
+        if cursor:
+            cursor.close()
+        if cnx and cnx.is_connected():
+            cnx.close()
+
+def log_form(username, email, password):
+    user = None
+    cnx = None
+    cursor = None
+    try:
+        cnx = mysql.connector.connect(
+            host = DB_HOST, user = DB_USER, password = DB_PASSWORD, database = DATABASE, port = DB_PORT
+        )
+        cursor = cnx.cursor(dictionary=True)
+        rqst = "SELECT id, username, email, password_hash FROM users WHERE username = %s AND email = %s"
+        cursor.execute(rqst, (username, email))
+        user = cursor.fetchone()
+        
+        if not user:
+            return "Неверное имя пользователя или email", False, None
+        if bcrypt.check_password_hash(user['password_hash'], password): 
+            return f"Добро пожаловать, {user['username']}!", True, user
+        else:
+            return "Неверный пароль", False, None
+            
+    except mysql.connector.Error as e:
+        return f"Произошла ошибка базы данных: {e}", False, None
+    except Exception as e:
+        return f"Произошла непредвиденная ошибка: {e}", False, None
+    finally:
+        if cursor:
+            cursor.close()
+        if cnx and cnx.is_connected():
+            cnx.close()
+
 def search_by_character(character_name):
     quotes  = None
     cnx = None
@@ -187,6 +215,8 @@ def search_by_title(anime_title):
     return quotes
 
 app = Flask(__name__)
+app.secret_key = 'sdj1u5B^7*6sDk90jH@lP8mN!oV3fTqX'
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 @app.route('/index.html')
@@ -201,18 +231,9 @@ def daily():
     
     return render_template("daily.html", daily_quote=daily_quote_data) 
 
-# @app.route('/favorites')
-# @app.route('/favorites.html')
-# def favorites():
-#     favorites_quotes_list = [] 
-#     return render_template("favorites.html", favorites_quotes=favorites_quotes_list)
-
-
-
 @app.route('/favorites.html')
 def favorites_page():
     return render_template('favorites.html')
-
 
 @app.route('/search-character', methods=['POST'])
 def handle_character_search():
@@ -234,15 +255,50 @@ def handle_anime_search():
     random_quotes = get_quotes()
     return render_template('index.html', random_quotes=random_quotes, search_result_character=None, search_result_anime=found_quotes)
 
+@app.route('/register.html')
+def register_page():
+    return render_template('register.html')
 
+@app.route('/register', methods=['POST'])
+def handle_register():
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    if not username or not email or not password:
+        flash('Пожалуйста, заполните все поля.', 'error')
+        return redirect(url_for('register_page'))
+    message, success = reg_form(username, email, password)
+    if success:
+        flash(message, 'success')
+        return redirect(url_for('login_page')) 
+    else:
+        flash(message, 'error')
+        return redirect(url_for('register_page'))
 
-# @app.route('/auth', methods=['GET', 'POST'])
-# def auth():
-#     try:
-#         if 
+@app.route('/login.html')
+def login_page():
+    return render_template('login.html')
 
+@app.route('/login', methods=['POST'])
+def handle_login():
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    if not username or not email or not password:
+        flash('Пожалуйста, заполните все поля.', 'error')
+        return redirect(url_for('login_page'))
+
+    message, success, user = log_form(username, email, password)
+    
+    if success:
+        flash(message, 'success')
+        return redirect(url_for('index')) 
+    else:
+        flash(message, 'error')
+        return redirect(url_for('login_page'))
 
 if __name__ == "__main__":
     # load_quotes()
     app.run(debug=True)
-
